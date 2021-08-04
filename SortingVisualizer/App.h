9708f708f2bcd1a22c06025d5c059b62c2a91cf7
Jaspal-Basra <61642475+Jaspal-Basra@users.noreply.github.com>
2021-08-03 23:14:57 -0700

    Cleaned up unnecessary code and added more comments

--- SortingVisualizer/App.h
+++ SortingVisualizer/App.h
@@ -8,15 +8,16 @@
 
 #include <iostream>
 #include <vector>
-#include <chrono>
-
-//#include "Bar.h"
 
 #define WINDOW_WIDTH    800
 #define WINDOW_HEIGHT   600
 
-#define NUMBER_OF_BARS  200
+// Non-recursive sorting algorithms are implemented using state machines to
+// prevent blocking the App's main loop
 
+/**
+* States for bubble sort state machine
+*/
 enum bubbleSortState
 {
 	BUBBLE_CHECK_CONDITION_OUTER,
@@ -28,6 +29,9 @@ enum bubbleSortState
 	BUBBLE_SORT_FINISHED,
 };
 
+/**
+* States for selection sort state machine
+*/
 enum selectionSortState
 {
 	SELECTION_CHECK_CONDITION_OUTER,
@@ -40,6 +44,9 @@ enum selectionSortState
 	SELECTION_SORT_FINISHED,
 };
 
+/**
+* States for insertion sort state machine
+*/
 enum insertionSortState
 {
 	INSERTION_CHECK_CONDITION_OUTER,
@@ -48,16 +55,15 @@ enum insertionSortState
 };
 
 /*
-    Class that acts as the game engine.
+    Class that acts as the app engine.
     Wrapper class.
 */
-
-class Game
+class App
 {
 public:
     // Constructors / Destructors
-    Game();
-    virtual ~Game();
+    App();
+    virtual ~App();
 
     // Public Methods
     void update();
@@ -67,73 +73,48 @@ public:
     const bool isWindowOpen() const;
     const sf::Vector2u getWindowSize() const;
 
-    // Variables
-
 private:
-    // Variables
+
+    // Private variables
+
     // Window
     sf::RenderWindow* window;
     sf::VideoMode videoMode;
     sf::Event event;
-
     sf::Vector2u windowBounds;
 
-    // Sorting state machine variables
-    bubbleSortState bubbleState;
-    selectionSortState selectionState;
-    insertionSortState insertionState;
+    // Sorting select variable
+    char sortingSelect;
 
-    // Sorting swap variables
-    sf::Vector2f aPos;
-    sf::Vector2f bPos;
-    sf::RectangleShape temp;
-
-    // Bubble sort variables
-    uint8_t bubbleSortOuterIndex;
-    uint8_t bubbleSortInnerIndex;
-    bool hasSwapOccurred; // Optimizes bubble sort by exiting early 
-
-    // Selection sort variables
-    uint8_t selectionSortOuterIndex;
-    uint8_t selectionSortInnerIndex;
-    uint8_t innerIndexMin;
-
-    // Insertion sort variables
-    uint8_t insertionSortOuterIndex;
-    uint8_t insertionSortInnerIndex;
-
-    // Merge sort vairbales
-    bool renderM;
-    bool last;
-
-    // Quick sort variabled
-
-    char c;
-
-    std::vector<float> barPositions;
-    std::vector<sf::RectangleShape> barsBuf;
-
-	std::vector<float> barHeights;
-	std::vector<float> barHeights2;
-
-	int barIndex, index1, cnt2;
-
+    // Bar object
     sf::RectangleShape bar;
+
+    // Bar vector
     std::vector<sf::RectangleShape> bars;
 
+    // Bar properties
     unsigned int numberOfBars;
     float barSize;
-    unsigned int barCount;
 
+    sf::Color initialColor;
+    sf::Color comparisonColor;
+    sf::Color scanColor;
+    sf::Color partitionIndexColor;
+    sf::Color mergeColor;
+    sf::Color finalColor;
+
+    // Bar height vector
+	std::vector<float> barHeights;
+
+    // Flag to indicate sorting has finished
     bool sortFinished;
+
+private:
 
     // Private methods
     void initBars();
     void initializeVariables();
     void initWindow();
-    int partition(std::vector<sf::RectangleShape>& A, int start, int end);
-    void merge(std::vector<sf::RectangleShape>& A, std::vector<sf::RectangleShape>& left, std::vector<sf::RectangleShape>& right, int startLeft, int endLeft, int startRight, int endRight);
-    void swap(std::vector<sf::RectangleShape>& A, int a, int b);
 
     void pollEvents();
     
@@ -141,12 +122,18 @@ private:
     
     void renderBars(sf::RenderTarget& target);
 
+    // Sorting functions
     void quickSort(std::vector<sf::RectangleShape>& A, int start, int end);
     void mergeSort(std::vector<sf::RectangleShape>& A, int start, int end);
     void insertionSort(std::vector<sf::RectangleShape>& A, int n);
     void bubbleSort(std::vector<sf::RectangleShape>& A, int n);
     void selectionSort(std::vector<sf::RectangleShape>& A, int n);
 
+    // Sorting helper functions
+    int partition(std::vector<sf::RectangleShape>& A, int start, int end);
+    void merge(std::vector<sf::RectangleShape>& A, std::vector<sf::RectangleShape>& left, std::vector<sf::RectangleShape>& right, int startLeft, int endLeft, int startRight, int endRight);
+    void swap(std::vector<sf::RectangleShape>& A, int a, int b);
+
+    // Rendering functions (for recursive sorting algorithms that block the main loop)
     void renderMergeSort(sf::RenderTarget& target, std::vector<sf::RectangleShape>& A, int startLeft, int endRight);
-    void renderColor(sf::RenderTarget& target);
 };
